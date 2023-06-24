fd8b8a8765d338c715d74899385d0cb393081e76
Ivan <ivanvolodin960@gmail.com>
2023-06-25 00:34:59 +0300

    feat: create component Layout

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Head from 'next/head';
+import React, { FC, ReactNode, useEffect, useState } from 'react'
+import FavIcon from '../../assets/images/favicon.png';
 
-export default function Layout() {
+interface ILayoutProps {
+  title: string
+  children: ReactNode
+}
+
+const Layout: FC<ILayoutProps> = ({ title, children }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true)
+    const timeout = setTimeout(() => { setIsLoading(false) }, 4000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, []);
+
   return (
     <div>
-      Layout
+      <Head>
+        <title>{title} | Taxi App</title>
+        <meta name="description" content="Taxi App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/png" href={FavIcon.src} />
+      </Head>
+
+      {children}
     </div>
   )
 }
+
+export default Layout;
