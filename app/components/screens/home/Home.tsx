67c0f49fdf23e026ca3e6c447664e8e8b3dffb57
Ivan <ivanvolodin960@gmail.com>
2023-06-26 00:48:48 +0300

    feat: create component Map

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from '../../layout/Layout';
+import Map from './Map';
 
 const Home = () => {
   return (
     <Layout title={'Taxi App'}>
-      <div>Home</div>
+      <div>
+        <Map />
+      </div>
     </Layout>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MAP_KEY: 'fjuergyvfuiv234'
+  }
 }
 
 module.exports = nextConfig
